fe53ece8e488db010f2574a15b653c91beba583e
Felipe Marques de Souza <felipehost@hotmail.com>
2023-09-28 21:19:18 -0300

    hello_cargo project have been used to study exercism projects.

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,3 +1,41 @@
 fn main() {
-    println!("Hello, world!");
+
+    let n = 10_000;
+    let mut count = 0;
+    let mut index = 0;
+    let mut n_prim = 0;
+    let mut is_prim = false;
+
+    loop {
+        (n_prim, is_prim) = is_prime(count);
+        if is_prim {
+            println!("{count} é primo");
+            if index == n {
+                break;
+            } else {
+                index += 1;
+            }
+        } else {
+            println!("{count} não é primo");
+        }
+        count += 1;
+    }
+
+    println!("{}", n_prim);
+}
+
+pub fn is_prime(x: u32) -> (u32, bool) {
+
+    if x <= 1 {
+        return (0, false);
+    }
+
+    for i in 2..=(x / 2) {
+        if x % i == 0 {
+            // println!("{x} é divisivel por {i}");
+            return (0, false);
+        }
+    }
+
+    (x, true)
 }
